43704bc0c1213e6c513191ed632a6fd08a37442a
arifializevic <arifializevic.m.maribondang@sat.co.id>
2024-11-15 11:45:18 +0700

    update

--- Data Mining/Crawling Youtube.py
+++ Data Mining/Crawling Youtube.py
@@ -53,7 +53,7 @@ def video_comments(video_id, apikey):
 # Video id nya
 # https://www.youtube.com/watch?v=lWp4r4Y2A58
 video_id = "lWp4r4Y2A58"
-apikey = 'AIzaSyDrwVcjd1hQpsivM11bq996l1zn9xj5r38'
+# apikey = 'AIzaSyDrwVcjd1hQpsivM11bq996l1zn9xj5r38'
 
 # jalankan function
 comments = video_comments(video_id, apikey)
